2df5bf88c3e05fb2e5ebaadc4ab9fce004e83d95
DIGIKRASH <digitallkrash@gmail.com>
2022-09-13 21:57:46 +0600

    register, personal, preloader

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 $('.search').click(function () {
   $('.search__message').toggleClass('active');
 });
+
+$('.users__item-avatar').click(function () {
+  $('.sidebar__personal').toggleClass('active');
+});
