533f66fb225b1658156a2907d16f0c34d4dc6e9f
DIGIKRASH <digitallkrash@gmail.com>
2022-09-14 20:55:51 +0600

    search, settings, empty

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,15 @@ $('.search').click(function () {
 $('.users__item-avatar').click(function () {
   $('.sidebar__personal').toggleClass('active');
 });
+$('.add').click(function () {
+  $('.search-users').toggleClass('active');
+});
+$('.settings__btn').click(function () {
+  $('.settings--left').toggleClass('active');
+  $('.settings--right').toggleClass('active');
+});
+
+$('.back').click(function () {
+  $(this).parent().removeClass('active');
+  $('.settings--right').removeClass('active');
+});
